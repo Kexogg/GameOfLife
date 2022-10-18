643c37dfda16847c6a0c2b62abc9cd0ffb00f6f2
Kexogg <Kexogg@gmail.com>
2022-10-18 19:42:30 +0500

    moved engine to class library

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace GameOfLife;
 
+using GameOfLifeEngine;
 class GameOfLife
 {
     static void Main()
@@ -8,13 +9,14 @@ class GameOfLife
         PrintGrid(grid);
         for (int i = 0; i < 200; i++)
         {
-            ProcessIteration(CalculateChanges(grid), grid);
-            if (!CheckForAliveCells(grid)) break;
+            GameOfLifeEngine.ProcessIteration(GameOfLifeEngine.CalculateChanges(grid), grid);
+            if (!GameOfLifeEngine.CheckForAliveCells(grid)) break;
         }
         Console.WriteLine("Game finished");
         PrintGrid(grid);
     }
-    public static bool[,] SetupGrid()
+
+    private static bool[,] SetupGrid()
     {
         var userInput = GetDimensions();
         var grid = new bool[userInput[0], userInput[1]];
@@ -40,66 +42,7 @@ class GameOfLife
         }
     }
 
-    public static bool CheckForAliveCells(bool[,] grid)
-    {
-        var aliveCells = 0;
-        foreach (var cell in grid)
-        {
-            if (cell)
-                aliveCells++;
-        }
-
-        return aliveCells != 0;
-    }
-    
-    public static int[][] CalculateChanges(bool[,] grid)
-    {
-        var changes = new List<int[]>();
-        for (int i = 0; i < grid.GetLength(0); i++)
-        {
-            for (int j = 0; j < grid.GetLength(1); j++)
-            {
-                var neighbors = 0;
-                for (int k = 0; k < 3; k++)
-                {
-                    for (int l = 0; l < 3; l++)
-                    {
-                        if (k == 1 & l == 1) continue;
-                        if (ParseCell(i + k - 1, j + l - 1, grid))
-                            neighbors++;
-                    }
-                }
-                if (neighbors is not (2 or 3) && grid[i, j])
-                    changes.Add(new []{i, j});
-                else if (neighbors is 3 && !grid[i, j])
-                    changes.Add(new []{i, j});
-            }
-        }
-        return changes.ToArray();
-    }
-
-    public static bool ParseCell(int coordinateX, int coordinateY, bool[,] grid)
-    {
-        if (coordinateX < 0)
-            coordinateX = grid.GetLength(0) - 1;
-        else if (coordinateX == grid.GetLength(0))
-            coordinateX = 0;
-        if (coordinateY < 0)
-            coordinateY = grid.GetLength(1) - 1;
-        else if (coordinateY == grid.GetLength(0))
-            coordinateY = 0;
-        return grid[coordinateX, coordinateY];
-    }
-
-    public static void ProcessIteration(int[][] changes, bool[,] grid)
-    {
-        foreach (var coordinates in changes)
-        {
-            grid[coordinates[0], coordinates[1]] = !grid[coordinates[0], coordinates[1]];
-        }
-    }
-
-    public static void PrintGrid(bool[,] grid)
+    private static void PrintGrid(bool[,] grid)
     {
         for (int i = 0; i < grid.GetLength(0); i++)
         {
---
+++ GameOfLifeEngine/Engine.cs
@@ -0,0 +1,63 @@
+﻿namespace GameOfLifeEngine;
+
+public class GameOfLifeEngine
+{
+    public static void ProcessIteration(int[][] changes, bool[,] grid)
+    {
+        foreach (var coordinates in changes)
+        {
+            grid[coordinates[0], coordinates[1]] = !grid[coordinates[0], coordinates[1]];
+        }
+    }
+    
+    public static int[][] CalculateChanges(bool[,] grid)
+    {
+        var changes = new List<int[]>();
+        for (var i = 0; i < grid.GetLength(0); i++)
+        {
+            for (var j = 0; j < grid.GetLength(1); j++)
+            {
+                var neighbors = 0;
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int l = 0; l < 3; l++)
+                    {
+                        if (k == 1 & l == 1) continue;
+                        if (ParseCell(i + k - 1, j + l - 1, grid))
+                            neighbors++;
+                    }
+                }
+                if (neighbors is not (2 or 3) && grid[i, j])
+                    changes.Add(new []{i, j});
+                else if (neighbors is 3 && !grid[i, j])
+                    changes.Add(new []{i, j});
+            }
+        }
+        return changes.ToArray();
+    }
+
+    private static bool ParseCell(int coordinateX, int coordinateY, bool[,] grid)
+    {
+        if (coordinateX < 0)
+            coordinateX = grid.GetLength(0) - 1;
+        else if (coordinateX == grid.GetLength(0))
+            coordinateX = 0;
+        if (coordinateY < 0)
+            coordinateY = grid.GetLength(1) - 1;
+        else if (coordinateY == grid.GetLength(0))
+            coordinateY = 0;
+        return grid[coordinateX, coordinateY];
+    }
+    
+    public static bool CheckForAliveCells(bool[,] grid)
+    {
+        var aliveCells = 0;
+        foreach (var cell in grid)
+        {
+            if (cell)
+                aliveCells++;
+        }
+
+        return aliveCells != 0;
+    }
+}
